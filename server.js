dd5c36ca9ec6fbad76db6e3501a0b7f3106110ad
Sapphire-Coder <74931786+Sapphire-Coder@users.noreply.github.com>
2022-03-04 15:21:32 -0500

    I can view a list of my Pokemon

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const app = express()
 const port = 3000
 const pokemon = require('./models/pokemon')
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!')
 })
 
-app.listen(port, () => console.log(`Listening on port: ${port}`))
-
 app.get('/pokemon', (req, res) => {
-    res.send(pokemon)
+    res.render('Index', {pokemon: pokemon})
 })
+
+
+
+app.listen(port, () => console.log(`Listening on port: ${port}`))
